ef528306f33b6d1ee2bcb05e665836db8826ebaa
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2025-01-08 15:18:29 +0500

    Initial Commit

---
+++ CoinTracker/CoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerApp.swift
+//  CoinTracker
+//
+//  Created by Dias Yerlan on 08.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
